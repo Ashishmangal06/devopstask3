b94b7f3624b9eb680c3bf29aa56ebba3a221d937
Ashishmangal06 <59526244+Ashishmangal06@users.noreply.github.com>
2020-06-23 17:03:57 +0530

    send mail

---
+++ mail.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+	port = 587  
+	smtp_server = "smtp.gmail.com"
+	sender_email = "ashishmangal925@gmail.com"
+	receiver_email = "ashishmangal925@gmail.com"
+	password = input("Enter you password:")
+	message = """\
+	Subject: Warning """	
+
+	context = ssl.create_default_context()
+	with smtplib.SMTP(smtp_server, port) as server:
+	    server.ehlo()  
+	    server.starttls(context=context)
+	    server.ehlo() 
+	    server.login(sender_email, password)
+
+	    
+        server.sendmail(sender_email, receiver_email, message)
